70ba525418c9b182ed9aece71a53e709848774c4
Alex Eisenach <hoopmiester@Alexs-MacBook-Pro-3.local>
2013-11-21 17:26:27 -0600

    added js files

--- ai.js
+++ ai.js
@@ -1,119 +1,77 @@
 function center() {
-	for(var i = 0; i < game.board.length; i++) {
-		if(game.board[i] == 4) {
-			return true;
-		}
-	}
-	return false;
-
-
+    for(var i = 0; i < game.board.length; i++) {
+       	if(game.board[i] == 4) {
+            return true;
+        }
+    }
+    return false;
 }
-
-
-
-
-
 function combo_match_2(arr, bigger_arr) {
-	match = 0;
-	result = [];
-	for(var i = 0; i < bigger_arr.length; i++) {
-		if(arr[0] == bigger_arr[i]) {
-			match += 1;
-			result.push(bigger_arr[i])
-		}
-		if(arr[1] == bigger_arr[i]) {
-			match += 1;
-			result.push(bigger_arr[i])
+    match = 0;
+    result = [];
+        for(var i = 0; i < bigger_arr.length; i++) {
+                if(arr[0] == bigger_arr[i]) {
+                    match += 1;
+                    result.push(bigger_arr[i])
+                }
+                if(arr[1] == bigger_arr[i]) {
+	                match += 1;
+	                result.push(bigger_arr[i])
 
-		}
-		if(arr[2] == bigger_arr[i]) {
-			match += 1;
-			result.push(bigger_arr[i])
-
-		}
-		
-	}
-	if(match == 2) {
-		return true;
+                }
+                if(arr[2] == bigger_arr[i]) {
+                    match += 1;
+                    result.push(bigger_arr[i])
+                }             
+        }
+        if(match == 2) {
+            return true;
+        }
+        else {
+            return false;
+        }
+}
+function removeSub(arr, index) {
+	arr.splice(index,1);
+}
+function closeCompetition(player) {
+	if(player == player_1) {
+	    opposing_player = player_2;
 	}
 	else {
-		return false;
+	    opposing_player = player_1;
+	}
+	for(var i = 0; i < game.winning_combos.length; i++) {
+	    if(combo_match_2(game.winning_combos[i], player.moves)) {
+	        potential_win = game.winning_combos[i];
+	        for(var i = 0; i < opposing_player.moves.length; i++) {
+                if(potential_win[0] == opposing_player.moves[i]) {
+                    console.log("lost on 1")
+                    break;
+                }
+                if(potential_win[1] == opposing_player.moves[i]) {
+                    console.log("lost on 2")
+                    break;
+                }        
+                if(potential_win[2] == opposing_player.moves[i]) {
+                    console.log("lost on 3")
+                    break;
+                }
+			}	
+			for(var i = 0; i < player.moves.length; i++) {
+		        if(potential_win[0] == player.moves[i]) {
+		            removeSub(potential_win, 0);
+		        }
+		        if(potential_win[1] == player.moves[i]) {
+		            removeSub(potential_win, 1);
+		        }
+		        if(potential_win[2] == player.moves[i]) {
+		            removeSub(potential_win, 2);
+		        }
+    		}
+	        close_comp = potential_win[0];
+	        return true;
+	    }
 	}
 }
-
-function removeSub(arr, index) {
-
-arr.splice(index,1);
-
-
-}
-
-
-
-function closeCompetition(player) {
-		if(player == player_1) {
-			opposing_player = player_2;
-		}
-		else {
-			opposing_player = player_1;
-		}
-		for(var i = 0; i < game.winning_combos.length; i++) {
-
-			if(combo_match_2(game.winning_combos[i], player.moves)) {
-				potential_win = game.winning_combos[i];
-				console.log("potential_win: " + potential_win)
-				console.log("player moves: " + player.moves)
-				console.log("opposing player moves: " + opposing_player.moves)
-
-
-					for(var i = 0; i < opposing_player.moves.length; i++) {
-					if(potential_win[0] == opposing_player.moves[i]) {
-						console.log("lost on 1")
-						break;
-					}
-					if(potential_win[1] == opposing_player.moves[i]) {
-						console.log("lost on 2")
-						break;
-					}	
-					if(potential_win[2] == opposing_player.moves[i]) {
-						console.log("lost on 3")
-						break;
-					}
-				}
-
-
-
-
-
-				for(var i = 0; i < player.moves.length; i++) {
-					if(potential_win[0] == player.moves[i]) {
-						removeSub(potential_win, 0);
-					}
-					if(potential_win[1] == player.moves[i]) {
-						removeSub(potential_win, 1);
-
-					}
-					if(potential_win[2] == player.moves[i]) {
-						removeSub(potential_win, 2);
-
-					}
-				}
-				console.log("final;" + potential_win);
-
-				close_comp = potential_win[0];
-
-				return true;
-			}
-
-		}
-
-
-}
-
-
-
-
-
-
-
 
--- scripts.js
+++ scripts.js
@@ -1,185 +1,132 @@
 
-var count = 0;
-var next_move;
+var count = 1;
 var close_comp;
 var opposing_player;
-
-
 function Player(name, marker) {
-	this.name = name;
-	this.marker = marker;
-	this.moves = [];
+        this.name = name;
+        this.marker = marker;
+        this.moves = [];
 }
-
-
-
-
-
-
 function combo_match(arr, bigger_arr) {
-	var length = bigger_arr.length;
-	for(var i = 0; i < length; i++) {
-		if(arr[0] == bigger_arr[i]) {
-			for(var i = 0; i < bigger_arr.length; i ++) {
-				if(arr[1] == bigger_arr[i]) {
-					for(var i = 0; i < bigger_arr.length; i ++) {
-						if(arr[2] == bigger_arr[i]) {
-							return true;
-						}
-					}
-				}	
-			}
-		}
-
-	}
-	return false;
+        var length = bigger_arr.length;
+        for(var i = 0; i < length; i++) {
+                if(arr[0] == bigger_arr[i]) {
+                        for(var i = 0; i < bigger_arr.length; i ++) {
+                                if(arr[1] == bigger_arr[i]) {
+                                        for(var i = 0; i < bigger_arr.length; i ++) {
+                                                if(arr[2] == bigger_arr[i]) {
+                                                        return true;
+                                                }
+                                        }
+                                }        
+                        }
+                }
+        }
+        return false;
 }
-
-
-
 function Game(player_1, player_2) {
-	this.board = [0,1,2,3,4,5,6,7,8];
-	this.winning_combos = [
-			[0,3,6],
-			[1,4,7],
-			[2,5,8],
-			[0,1,2],
-			[3,4,5],
-			[6,7,8],
-			[6,4,2],
-			[0,4,8]];
+        this.board = [0,1,2,3,4,5,6,7,8];
+        this.winning_combos = [
+                        [0,3,6],
+                        [1,4,7],
+                        [2,5,8],
+                        [0,1,2],
+                        [3,4,5],
+                        [6,7,8],
+                        [6,4,2],
+                        [0,4,8]];
 
-	this.playerWon = function(player) {
+    this.playerWon = function(player) {
+		winning_combos = [
+	        [0,3,6],
+	        [1,4,7],
+	        [2,5,8],
+	        [0,1,2],
+	        [3,4,5],
+	        [6,7,8],
+	        [6,4,2],
+	        [0,4,8]
+	    ];
+        for(var i = 0; i < 8; i++) {
+            if(combo_match(winning_combos[i], player.moves)) {
+               	return true;
+            }
+        }
 
-	winning_combos = [
-			[0,3,6],
-			[1,4,7],
-			[2,5,8],
-			[0,1,2],
-			[3,4,5],
-			[6,7,8],
-			[6,4,2],
-			[0,4,8]];
-
-		for(var i = 0; i < 8; i++) {
-			if(combo_match(winning_combos[i], player.moves)) {
-				return true;
-			}
-		}
-
-
+    }
+    this.makeMove = function(player, move) { 
+	    player.moves.push(move);
+	    console.log("player move " + move)
+	    this.remove(move);
 	}
-
-
-
-	this.makeMove = function(player, move) { 
-
-
-	player.moves.push(move);
-	console.log("player move " + move)
-	this.remove(move);
-
-}
-
-	this.moveTaken = function(attempted_move, player) {
-		for (var i = 0; i < player.moves.length; i++) {
-			if(attempted_move == player.moves[i]) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	this.remove = function(move) {
-		for(var i = 0; i < this.board.length; i++) {
-			if(this.board[i] == move) {
-				this.board.splice(i,1)
-				return;
-			}
-		}
-	}
-
-	this.playGame = function() {
-
-if(count % 2 == 0) {
-		current_player = player_1;
-		$("#status").html(current_player.name + "'s turn");
-		if(closeCompetition(player_1)) {
-			var move = close_comp;
-		}
-		else if(center()) {
-		    var move = 4;
-		}
-		else if(closeCompetition(player_2)) {
-			var move = close_comp;
-		}
-		else {
-		var move = game.board[0];
-		}
-
-		if (game.moveTaken(move,player_1)  && game.moveTaken(move,player_2)) {
-			game.makeMove(current_player, move)
-			$("td#" + move).html(current_player.marker);
-			count+= 1;
-			console.log(current_player.moves);
-		}
-		else {
-			 $("#status").html("That move has been taken")
-		}
-		if(game.playerWon(current_player)) {
-			$("#status").html(current_player.name + "wins");
-
+    this.moveTaken = function(attempted_move, player) {
+        for (var i = 0; i < player.moves.length; i++) {
+            if(attempted_move == player.moves[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    this.remove = function(move) {
+       	for(var i = 0; i < this.board.length; i++) {
+	        if(this.board[i] == move) {
+	        	this.board.splice(i,1)
+	            return;
+	        }
+        }
+    }
+    this.executeMove = function(move) {
+	    if(game.moveTaken(move,player_1)  && game.moveTaken(move,player_2)) {
+            game.makeMove(current_player, move)
+            $("td#" + move).html(current_player.marker);
+            count+= 1;
+            console.log(current_player.moves);
+        }
+        else {
+        	$("#status").html("That move has been taken")
+        }
+        if(game.playerWon(current_player)) {
+       		$("#status").html(current_player.name + "wins");
 			return;
-		}
+        }
+        else {
+        	game.playGame();
+        }
+    }
+
+    this.playGame = function() {
+		if(count % 2 == 0) {
+	        current_player = player_1;
+	        $("#status").html(current_player.name + "'s turn");
+	        if(closeCompetition(player_1)) {
+	                var move = close_comp;
+	        }
+	        else if(center()) {
+	            var move = 4;
+	        }
+	        else if(closeCompetition(player_2)) {
+	                var move = close_comp;
+	        }
+	        else {
+	        var move = game.board[0];
+	        }
+			game.executeMove(move);
+			}
 		else {
-			game.playGame();
+			current_player = player_2;
+	   	 	$("#status").html(current_player.name + "'s turn");
+	   		$("td").click(function() {
+	    		var move = parseInt(($(this).attr("id")));
+	     		game.executeMove(move);
+	   		})
 		}
-
-	}
-
-	else {
-		current_player = player_2;
-		$("#status").html(current_player.name + "'s turn");
-		$("td").click(function() {
-			var move = parseInt(($(this).attr("id")));
-			if (game.moveTaken(move,player_1)  && game.moveTaken(move,player_2)) {
-				game.makeMove(current_player, move)
-				$("td#" + move).html(current_player.marker);
-				count+= 1;
-				console.log(count);
-			}
-			else {
-				 $("#status").html("That move has been taken")
-			}
-			if(game.playerWon(current_player)) {
-				$("#status").html(current_player.name + " wins")
-
-				return;
-			}
-			else {
-				game.playGame();
-			}
-
-		})
-
-	}
-
-	}
-
+   	 }
 }
 
 $(document).ready(function() {
-
-	player_1 = new Player("Computer", "X");
-	player_2 = new Player("You", "O")
-	game = new Game(player_1, player_2);
-	game.playGame();
-	
-
+        player_1 = new Player("Computer", "X");
+        player_2 = new Player("You", "O")
+        game = new Game(player_1, player_2);
+        game.playGame();        
 })
-
-
-
-
-
 
